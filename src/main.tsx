6336381d4fa9bd48bf4288ccf6d22a521510a58d
Shabuuu-1234 <shabnam.ofl@gmail.com>
2026-05-17 12:21:41 +0530

    fix: use router export instead of getRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
+import { router } from "./router";
 import "./styles.css";
 
-const router = getRouter();
+<RouterProvider router={router} />
 
 declare module "@tanstack/react-router" {
   interface Register {
