196e6d7226ef9c14df53afd349dad675973168dc
Shabuuu-1234 <shabnam.ofl@gmail.com>
2026-05-17 11:55:45 +0530

    fix: use app.getAppPath() for Electron file loading

--- electron/main.js
+++ electron/main.js
@@ -128,7 +128,7 @@ function createWindow() {
     mainWindow.loadURL("http://localhost:5173");
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, "..", "dist", "index.html"));
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist", "index.html"));
   }
 
   mainWindow.once("ready-to-show", () => {
